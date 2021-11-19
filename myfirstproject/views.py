90929f3044eb017b69cc0aea8b8da8cd63e76954
GITisDick <F__ckYourselfGIT@withdildo.com>
2021-11-19 03:47:01 +0300

    Add words count feature

--- myfirstproject/views.py
+++ myfirstproject/views.py
@@ -3,5 +3,8 @@ def page(request):
 	return render(request,'page.html')
 def reverse(request):
         user_input_text=request.GET["userinputtext"]
+        count=len(user_input_text.split())
         reversed_text=user_input_text[::-1]
-        return render(request,'reverse.html',{"UIT":user_input_text,"RT":reversed_text})
+        return render(request,'reverse.html',{"UIT":user_input_text,
+                                              "RT":reversed_text,
+                                              "C":count,})
